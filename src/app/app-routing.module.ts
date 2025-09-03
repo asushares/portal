0fc8cd55280a34178d3966139fd27271798d665e
Preston Lee <preston.lee@prestonlee.com>
2025-09-02 17:40:53 -0700

    Stubbing patient area

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { BuilderComponent } from './builder/builder.component';
 import { SettingsComponent } from './settings/settings.component';
 import { SandboxComponent } from './sandbox/sandbox.component';
 import { SimulatorComponent } from './simulator/simulator.component';
+import { PatientComponent } from './patient/patient-component';
 
 
 const routes: Routes = [
@@ -13,7 +14,8 @@ const routes: Routes = [
   { path: 'builder/:consent_id', component: BuilderComponent },
   { path: 'simulator/:consent_id', component: SimulatorComponent },
   {path: 'settings', component: SettingsComponent},
-  {path: 'sandbox', component: SandboxComponent}
+  {path: 'sandbox', component: SandboxComponent},
+  {path: 'patient', component: PatientComponent}
 ]
 
 @NgModule({
